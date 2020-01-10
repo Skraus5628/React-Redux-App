303440a94fc4e90ae030feb1d0a1a20067e84fd8
Samuel Kraus <S_read@aol.com>
2020-01-09 22:49:29 -0600

    build

---
+++ poke-app/build/precache-manifest.efa9d8e0edd32fb8959ae4e12e322103.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae869db57ecc0f36980a2b8aaca517e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f459de628e9dcb5974b",
+    "url": "/static/css/main.5f9d53d4.chunk.css"
+  },
+  {
+    "revision": "76e880b45da3089effdb",
+    "url": "/static/js/2.de8ffa19.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.de8ffa19.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f459de628e9dcb5974b",
+    "url": "/static/js/main.72104077.chunk.js"
+  },
+  {
+    "revision": "eaf2edcdf6a738e76eea",
+    "url": "/static/js/runtime-main.94604acd.js"
+  }
+]);
